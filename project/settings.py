4b08c4409c2ebb37eb443bb012f719d869ad642a
KuthikBohdan1 <bkuzik14@gmail.com>
2025-06-06 14:57:55 +0300

    345

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(ypt1bfveo%cpm-vj_x8+@spd!zs7(-=b)2@d1uo%71l#zh%wx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost","127.0.0.1"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "tamplates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
